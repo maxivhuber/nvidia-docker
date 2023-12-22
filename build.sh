60359b6c651c8eaa40e1d36318c4e7a7ee4caa64
Maximilian Huber <>
2023-12-22 09:51:20 +0100

    🐛 Fixed container name for podman stop

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 
 PROJECT_NAME=$(grep -m 1 '^PROJECT_NAME=' .env | cut -d '=' -f2)
 
-podman stop ml-lab-0 > /dev/null 2>&1 || true && \
+podman stop "$PROJECT_NAME"-0 > /dev/null 2>&1 || true && \
 podman build --build-arg-file=./argfile.conf --tag ml-lab:latest -f ./Dockerfile > /dev/null 2>&1 && \
 podman images -f dangling=true -q | xargs --no-run-if-empty podman rmi > /dev/null 2>&1 && \
 podman run -d --rm --name "$PROJECT_NAME"-0 \
